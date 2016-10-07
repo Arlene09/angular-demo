5bf47a0a5a200cee8006076c453a28aba8b7fedd
os-shaozq <os-shaozq@jsfund.cn>
2016-10-07 13:30:16 +0800

    readme.md文件补充

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-项目启动步骤
-1、npm install
-2、npm install -g bower
-3、bower install
-4、gulp
+# angular-demo
+
+##项目启动步骤
+1.npm install
+2.npm install -g bower
+3.bower install
+4.gulp
+
+##用户登录信息
+* 登录名:   1@1.cn
+* 密码:    123456
 
 
-登录名:   1@1.cn
-密码:    123456# angular-demo
+##主要实现
+* angular基本框架
+* 根据文件类型将其进行分类
+* angular路由间的跳转
+* 列表展示 & 查看详情
+* 页面间的传参
+* 基本语法,如点击事件,ng-if等的使用
+* 将所有请求接口封装成服务
+* 对接口返回的数据统一处理
+* 常量的定义
+* 对notify提示插件的使用
+* 使用sass编辑页面样式
+* 通过gulp实现自动化工作流
+
+
--- src/js/config.js
+++ src/js/config.js
@@ -4,6 +4,65 @@
 
       //API统一为JSON格式
       $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
+      
+      
+      $httpProvider.interceptors.push(function ($q) {
+        return {
+          'request': function (config) {
+
+            if (config.hasOwnProperty("headers") && config.headers.hasOwnProperty("token")) {
+              //为所有发出请求的接口都带上token(token从后台获取到,存入本地)
+              config.headers.token = 'XXXX';
+            }
+            return config;
+          },
+
+          'response': function (response) {
+            if (!!response.data && response.data.hasOwnProperty('status')
+              && response.data.status.hasOwnProperty('code') && response.data.status.code == 10000) {
+              return response;
+            } else if (!!response.data && response.data.hasOwnProperty('status')
+              && response.data.status.hasOwnProperty('code') && response.data.status.code != 10000) {
+              return $q.reject(response.data.status.message);
+            } else {
+              return response;
+            }
+
+          },
+
+          'responseError': function (response) {
+            switch (response.status) {
+              case 400:
+              {
+                console.error("服务器返回: 400");
+                return $q.reject("网络不畅");
+              }
+              case 401:
+              {
+                console.error("服务器返回: 401" + angular.toJson(response));
+                return $q.reject(response.data.status.message);
+              }
+              case 403:
+              {
+                console.error("服务器返回: 403");
+                return $q.reject(response.data.status.message);
+              }
+              case 404:
+              {
+                console.error("服务器返回: 404");
+                return $q.reject("网络不畅");
+              }
+              case 500:
+              {
+                return $q.reject("网络不畅");
+              }
+              default:
+                return $q.reject("网络不畅");
+            }
+
+          }
+        };
+      });
 
     });
 
